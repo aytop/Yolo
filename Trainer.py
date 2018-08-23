a11fe0ea8cbde38ec5d312b1a997f597f1a69b60
Nikola Janicijevic <34868654+aytop@users.noreply.github.com>
2018-08-23 15:39:42 +0200

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -14,18 +14,20 @@ class Trainer:
     def train(self):
         print('Training started!')
         self.net.train()
+        loss = 0
         for index, dic in enumerate(self.data_loader):
             image, label = Variable(dic['image'].float()), Variable(dic['label'].float())
             image = image.cuda()
             label = label.cuda()
             self.optimizer.zero_grad()
             output = self.net(image)
-            loss = self.criterion(output, label)
+            loss += self.criterion(output, label)
             loss.backward()
             self.optimizer.step()
             if index % 10 == 0:
                 print('Train : [{}/{} ({:.0f}%)]'.format(index * len(image),
                                                                        len(self.data_loader.dataset),
                                                                        100. * index / len(self.data_loader)))
+                print('Avarage loss: ', loss/(index+1))
         print('Training finished!')
         torch.save(self.net.state_dict(), self.save)
