e69db1814860f8b66b0bfc12178722dc3033e6fb
aytop <nikola.jani97@gmail.com>
2018-08-23 14:05:55 +0200

    added iou

--- Loss.py
+++ Loss.py
@@ -34,14 +34,14 @@ class MyLoss(nn.Module):
         loss_conf_obj = \
             obj_coef * \
             target[:, 14, :, :] * \
-            torch.pow(f.sigmoid(target[:, 14, :, :]) - f.sigmoid(prediction[:, 14, :, :]), 2)
+            torch.pow(f.sigmoid(target[:, 14, :, :]*tensor_iou(prediction, target)) - f.sigmoid(prediction[:, 14, :, :]), 2)
 
         loss_conf_obj = loss_conf_obj.sum()
 
         loss_conf_noobj = \
             noobj_coef * \
             (1 - target[:, 14, :, :]) * \
-            torch.pow(target[:, 14, :, :] - prediction[:, 14, :, :], 2)
+            torch.pow(target[:, 14, :, :]*tensor_iou() - prediction[:, 14, :, :], 2)
 
         loss_conf_noobj = loss_conf_noobj.sum()
 
