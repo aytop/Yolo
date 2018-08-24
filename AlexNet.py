86c1bce627ecf54f6817568aeaf46d58040226bf
aytop <nikola.jani97@gmail.com>
2018-08-24 16:43:14 +0200

    alex

--- AlexNet.py
+++ AlexNet.py
@@ -11,10 +11,10 @@ model_urls = {
 class Alex(AlexNet):
     def __init__(self):
         super(Alex, self).__init__()
+        self.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
         for param in self.features.parameters():
             param.requires_grad = False
         self.classifier = nn.Conv2d(256, 15, kernel_size=3, padding=1)
-        self.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
 
     def forward(self, x):
         x = self.features(x)
