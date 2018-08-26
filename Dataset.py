117175196c8907869122f39e7e0d71fcec376281
Nikola Janicijevic <34868654+aytop@users.noreply.github.com>
2018-08-26 12:45:55 +0200

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -52,7 +52,7 @@ class Rescale(object):
 
 class DetectionDataSet(ds.Dataset):
     def __init__(self, paths='numpy/paths.txt', label_dir='numpy/', root_dir='train/',
-                 transform=transforms.Compose([Rescale((144*3, 72*9)), ToTensor()]),
+                 transform=transforms.Compose([Rescale((432, 432)), ToTensor()]),
                  abs_path='/home/aytop/PycharmProjects/Praksa/'):
         """
                 Args:
